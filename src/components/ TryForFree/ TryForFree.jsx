3004fda1a5edb6cfd06ef15c41829b161f0dfe51
dima-yurchuk <dimayurchuk20@gmail.com>
2022-02-18 10:29:29 +0200

    improving styles

--- src/components/ TryForFree/ TryForFree.jsx
+++ src/components/ TryForFree/ TryForFree.jsx
@@ -5,15 +5,11 @@ import {Link} from "react-router-dom";
 
 const TryForFree = () => {
     return (
-        <div>
-                <div className={'try-for-free'} style={{backgroundImage: `url(/images/tryForFree/bg.png)`}}>
-                    <div className={'try-for-free try-for-free__content'}>
-                        <div className={'try-for-free__text'}>
-                            Попробовать бесплатно
-                        </div>
-                        <img src='/images/tryForFree/arrow.png' className={'try-for-free__image'}/>
-                    </div>
+        <div className={'try-for-free'} style={{backgroundImage: `url(/images/tryForFree/bg.png)`}}>
+                <div className={'try-for-free__text'}>
+                    Попробовать бесплатно
                 </div>
+                <img src='/images/tryForFree/arrow.png' className={'try-for-free__image'}/>
         </div>
     );
 };
